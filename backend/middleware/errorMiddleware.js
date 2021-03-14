682147fe53677d52f1c5a879dde87a533599ee45
Yousef <yousefrizik92@gmail.com>
2021-03-14 14:28:59 +0200

    sets up backend folders, files, and adds errorHandlers

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -15,4 +15,21 @@ const errorHandler = (err, req, res, next) => {
   });
 };
 
-export { notFound, errorHandler };
+const developmentErrors = (err, req, res, next) => {
+  err.stack = err.stack || "";
+  const errorDetails = {
+    message: err.message,
+    status: err.status,
+    stack: err.stack,
+  };
+
+  res.status(err.status || 500).json(errorDetails);
+};
+
+const productionErrors = (err, req, res, next) => {
+  res.status(err.status || 500).json({
+    error: "Internal Server Error",
+  });
+};
+
+export { notFound, errorHandler, developmentErrors, productionErrors };
