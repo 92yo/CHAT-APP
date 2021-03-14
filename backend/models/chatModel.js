e8621c94dbf3fb484269cc2d8989b8b007611ab5
Yousef <yousefrizik92@gmail.com>
2021-03-14 15:01:29 +0200

    sets up chatroom model, user, and message. Sets up Mongoose Connection

---
+++ backend/models/chatModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const chatSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const Chat = mongoose.model("Chatroom", chatSchema);
+
+export default Chat;
