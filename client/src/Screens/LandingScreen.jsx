6020bb25e56cb82aa0a39c3a08ab3f133b921564
Yousef <yousefrizik92@gmail.com>
2021-04-27 12:46:04 +0300

    Adds new design for landing Page, adds Card component

--- client/src/Screens/LandingScreen.jsx
+++ client/src/Screens/LandingScreen.jsx
@@ -1,15 +1,13 @@
-// import { useState } from "react";
 import Meta from "../Components/Meta";
 import Hero from "../Components/Hero";
-
-// import Loader from "../Components/Loader";
-// import makeToast from "../Components/Toaster";
+import Card from "../Components/Card";
 
 const LandingScreen = () => {
   return (
     <>
-      <Hero />
       <Meta title="Chaitfy" />
+      <Hero />
+      <Card />
     </>
   );
 };
