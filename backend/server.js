27ef2970991031323826d633b9e7540440ddfe7c
Yousef <yousefrizik92@gmail.com>
2021-05-10 13:22:00 +0300

    fixes-heroku-bug

--- backend/server.js
+++ backend/server.js
@@ -49,14 +49,12 @@ app.use("/api/chatroom", chatRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const dirname = path.resolve();
-
 // Heroku
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
