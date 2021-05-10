ae2bed5281c15f2ac3b0395e5d8e8f72ab9cab3b
Yousef <yousefrizik92@gmail.com>
2021-05-10 11:24:41 +0300

    Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const scoketio = require("socket.io");
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
@@ -25,6 +26,12 @@ connectDB();
 const app = express();
 const server = http.createServer(app);
 
+const io = scoketio(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
 // Middlewares
 app.use(cors());
 app.use(express.json());
@@ -47,6 +54,53 @@ if (process.env.NODE_ENV === "production") {
     res.send("API is Running!");
   });
 }
+
+// Socket-io
+io.use(async (socket, next) => {
+  try {
+    const token = socket.handshake.query.token;
+    const payload = await jwt.verify(token, process.env.JWT_SECRET);
+    socket.userId = payload.id;
+    next();
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected: " + socket.userId);
+
+  socket.on("joinRoom", ({ chatroomId }) => {
+    socket.join(chatroomId);
+    console.log(`${socket.userId} joined chatroom: ${chatroomId} `);
+  });
+
+  socket.on("leaveRoom", ({ chatroomId }) => {
+    socket.leave(chatroomId);
+    console.log("A user left chatroom: " + chatroomId);
+  });
+
+  socket.on("chatroomMessage", async ({ chatroomId, message }) => {
+    if (message.trim().length > 0) {
+      const user = await User.findOne({ _id: socket.userId });
+      const newMessage = new Message({
+        chatroom: chatroomId,
+        user: socket.userId,
+        message,
+      });
+      io.to(chatroomId).emit("newMessage", {
+        message,
+        name: user.name,
+        userId: socket.userId,
+      });
+      await newMessage.save();
+    }
+  });
+
+  socket.on("disconnect", async () => {
+    console.log("Disconnected: " + socket.userId);
+  });
+});
 
 // Error Handlers
 app.use(notFound);
