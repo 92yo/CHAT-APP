79f5741e09024bcc814f806ecaab07946248fc9e
Yousef <yousefrizik92@gmail.com>
2021-05-11 11:56:10 +0300

    Heroku-8-fix

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.use("/api/chatroom", chatRoutes);
 
 // Heroku
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
